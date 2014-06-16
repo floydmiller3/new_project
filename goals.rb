fa43f25f46bd364f0dd5859cb2ae96da31162447
floydmiller3 <floydmiller3@gmail.com>
2014-06-16 19:53:48 -0400

    this is the first commit

---
+++ goals.rb
@@ -0,0 +1,7 @@
+My goals for the class
+
+1) build out my own app
+2) beat Hang's app 
+3) complete the class and not miss any of the classes 
+4) be proud of myself and my project
+5) Don't trip anyone with my power cord
